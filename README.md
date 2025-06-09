67c81786d45f4cf6590851282594fa46003b7b88
s7rid3r <lonestrider235@gmail.com>
2025-06-09 16:10:49 -0500

    add apache_redirector

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # red-team-infra
 This repository hosts Infrastructure-as-Code (IaC) to deploy various red team infrastructure, i.e., C2 servers, redirectors, etc.
-
+## Requirements
+For this code to work as it is written, you will need the following:
+- AWS Account
+- Cloudflare registered domain
 ## Getting Started
 ### Environment Variables
 Set AWS credentials:
@@ -20,29 +23,42 @@ Create a keypair in AWS and make sure the "aws_key_pair" and "ssh_key_path" vari
 aws_key_pair    =   "test"
 ssh_key_path    = "~/.ssh/test.pem"
 ```
-
 ### Ansible
 Download anisble collections:
 ```
 ansible-galaxy collection install --requirements-file ./sliver/ansible/requirements.yml
 ```
-
-### sliver
-This deploys a sliver team server in AWS.
+## sliver
+Deploys a sliver team server in AWS.
+### Setup
+Set all variables to your specifications in variables.auto.tfvars.
 
 You can change the operator configuration profiles in sliver/roles/operators/files/main.yml. Simply add or remove operator names in the file.
-
+### Deployment
 Deploy by running:
 ```
 cd ./sliver
 terraform init
 terrafrom apply
 ```
-
+### Usage
 Connect to the sliver server with:
 ```
 sliver-client import operator_configs/your/config/file
 sliver-client
 ```
+## apache_redirector
+Deploys an EC2 instance running an apache2 web server. Uses mod_rewrite rules to send all traffic to google.com unless it using a specified User-Agent.
+### Setup
+Set all variables to your specifications in variables.auto.tfvars.
+### Deployment
+Deploy by running:
+```
+cd ./apache_redirector
+terraform init
+terrafrom apply
+```
 ## What's Next
-I plan to continue working on developing some redirectors next.
+- Improvements to apache_redirector
+- Better documentation and guidance
+- nginx_redirector
---
+++ apache_redirector/main.tf
@@ -0,0 +1,123 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+
+    ansible = {
+      source  = "ansible/ansible"
+      version = "~>1.3.0"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "cloudflare" {
+  # token pulled from $CLOUDFLARE_API_TOKEN
+}
+
+resource "aws_security_group" "redirector" {
+  name = "redirector-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.aws_cidr_block]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "redirector" {
+  ami                    = var.aws_ami
+  instance_type          = var.aws_instance_type
+  key_name               = var.aws_key_pair
+  vpc_security_group_ids = [aws_security_group.redirector.id]
+
+  tags = {
+    Name = var.aws_instance_name
+  }
+}
+
+resource "ansible_host" "redirector" {
+  name   = aws_instance.redirector.public_ip
+  groups = ["redirector"]
+  variables = {
+    ansible_user                 = "ubuntu"
+    ansible_ssh_private_key_file = var.ssh_key_path
+    ansible_python_interpreter   = "/usr/bin/python3"
+    ansible_ssh_common_args      = "-o StrictHostKeyChecking=no"
+  }
+}
+
+resource "cloudflare_dns_record" "update_dns_record" {
+  zone_id = var.cloudflare_zone_id
+  content = aws_instance.redirector.public_ip
+  name    = var.cloudflare_name
+  proxied = true
+  ttl     = 1
+  type    = var.cloudflare_type
+}
+
+resource "null_resource" "wait_for_ssh" {
+  depends_on = [aws_instance.redirector]
+
+  provisioner "remote-exec" {
+    inline = ["echo 'SSH is up'"]
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.ssh_key_path)
+      host        = aws_instance.redirector.public_ip
+    }
+  }
+}
+
+resource "null_resource" "ansible_provision_local_exec" {
+  depends_on = [null_resource.wait_for_ssh]
+
+  triggers = {
+    instance_id = aws_instance.redirector.id
+  }
+
+  provisioner "local-exec" {
+    command    = <<EOT
+      ansible-playbook ansible/site.yml \
+        -i ansible/inventory.yml \
+        -e redirector_domain=${var.cloudflare_name} \
+        -e c2_domain=${var.c2_domain} \
+        -e c2_user_agent=${var.c2_user_agent}
+    EOT
+    on_failure = fail
+  }
+}
